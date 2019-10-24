383d2cc917b66d1c8fd501d6bf12b5edd292515a
sunmarkz <mailofsun@163.com>
2019-10-24 23:27:14 +0800

    damn it

--- Test/Canvas_03_BasicDiagram/Handler_text.js
+++ Test/Canvas_03_BasicDiagram/Handler_text.js
@@ -1,5 +1,5 @@
 
-const _tabText = ' '; // this is tab letter in textarea;
+const _tabText = '-'; // this is tab letter in textarea;
 const _startLocation = { x: 50, y: 50 };
 const _lineHeight = 10;
 const _lineWidth = 20;
@@ -7,12 +7,15 @@ const _gapping = 20;
 
 
 function t_Node(splitedString, lastNode = null) {
+
     //count how many tab have define which level is.
     this.level = Handle_text.FrontKeywordsCount(splitedString, _tabText);
+    console.log(this.level );
+    
     // this.content = Handle_text.FrontKeywordsFilter(splitedString, _tabText);
     this.content = splitedString;
 
-    
+
     // TODO : render text fulfill multiply line text
     // this.renderText = Handle_text.RenderTextInGraphic;
 
@@ -20,7 +23,7 @@ function t_Node(splitedString, lastNode = null) {
     // CanvStyle.Text();
 
     this.width = d.measureText(splitedString).width;
-    this.height = singleLetterHeight;
+    this.height = singleLetterHeight+_gapping;
 
     if (lastNode == null) {
         //first item initialize
@@ -31,91 +34,82 @@ function t_Node(splitedString, lastNode = null) {
         this.indexofAll = 0;
         this.x = _startLocation.x;
         this.y = _startLocation.y;
-        this.groupHeight = 0;
-        this.groupWidth = 0;
+
+        //group = this node's group;
+        this.group = new Graphic(this,'rect',this.x,this.y,this.width,this.height);
 
     } else {
-        
+
         if (lastNode.level == this.level) {
             // on same level 
             this.indexofLevel = lastNode.indexofLevel + 1;
             this.lastLevelLastItem = lastNode.lastLevelLastItem;
             this.indexofAll = lastNode.indexofAll + 1;
-            this.groupHeight = lastNode.groupHeight + this.height;
+            this.group = lastNode.group;//point to first item group;
 
-            lastNode.groupHeight = this.groupHeight;
-            // last level item height ++;
-
-            if(lastNode.lastLevelLastItem!=null){
-                lastNode.lastLevelLastItem.groupHeight = this.height + lastNode.lastLevelLastItem.groupHeight ; }
-
-
-            if (this.width > lastNode.groupWidth) {
-                // get biggest width in group
-                this.groupWidth = this.width;
-                lastNode.groupWidth = this.width;
-            }else{
-                this.groupWidth = lastNode.groupWidth;
+            this.group.height +=this.height;
+            if(this.lastLevelLastItem!=null){
+                this.lastLevelLastItem.y+=this.height/2;
+                // last level item height ++;
             }
 
+            if (this.width > this.group.width) {
+                // get biggest width in group
+                this.group.width = this.width;
+            } 
 
-            this.x = lastNode.x;
-            this.y = lastNode.y + lastNode.height;
+            this.x = this.group.x;
+            this.y = this.group.y + this.group.height;
+
         } else {
             //level change !
             if (this.level < lastNode.level) {
                 //upper level
-                this.indexofLevel = 0;// first item in level
-                this.lastLevelLastItem = lastNode.lastLevelLastItem.lastLevelLastItem;
-                this.indexofAll = lastNode.indexofAll+1;
-                this.groupHeight = lastNode.groupHeight + this.height;
-                lastNode.groupHeight = this.groupHeight;
+                this.indexofLevel = 0;
+                this.indexofAll = lastNode.indexofAll + 1;
 
-                if (this.width > lastNode.groupWidth) {
-                    // get biggest width in group
-                    this.groupWidth = this.width;
-                    lastNode.groupWidth = this.width;
-                }else{
-                    this.groupWidth = lastNode.groupWidth;     
-                }
-
-                 
-
-                this.x = lastNode.lastLevelLastItem.x;//this level's last node;
-                this.y = lastNode.lastLevelLastItem.y + lastNode.lastLevelLastItem.height;
+                this.lastLevelLastItem = this.levelLastItem(this.level-1);
+                var _lastone = this.levelLastItem(this.level);
+                console.log(_lastone);
+                this.group = _lastone.group;
                 
+                _lastone.group.height += lastNode.group.height;
+                this.x = _lastone.x;
+                this.y = this.group.y+this.group.height;
 
-            } else{
+            } else {
                 // next level
                 this.indexofLevel = 0;
                 this.lastLevelLastItem = lastNode;
-                this.indexofAll = lastNode.indexofAll+1;
-                this.groupWidth = this.width;
-                this.groupHeight = this.height;
-                this.x = lastNode.x+lastNode.groupWidth;
-                this.y = lastNode.y+ (this.groupHeight/2);
+                this.indexofAll = lastNode.indexofAll + 1;
+                this.x = lastNode.x + lastNode.group.width;
+                this.y = lastNode.y + this.height;
+                this.group = new Graphic(this, 'rect', this.x, this.y, this.width, this.height);
+                
 
             }
         }
     }
-    this.graphic = new Graphic('rect',this.x,this.y,this.width,this.height);
+    this.graphic = new Graphic(this, 'rect', this.x, this.y - _lineHeight * 2, this.width, this.height);
 }
-t_Node.prototype.draw = function(){
-    CanvStyle.Text();
-    
-    CanvDraw.t(this.content,this.x,this.y,this.width);
+
+t_Node.prototype.levelLastItem = function (level) {
+    if(this.level == level){
+        return this;
+    }
+    if(this.lastNode == null){
+        return null;
+    }
+    return this.lastNode.levelLastItem(level);
+}
+t_Node.prototype.draw = function () {
     this.graphic.draw();
+    CanvStyle.Node();
+    CanvStyle.Text();
 
+    CanvDraw.t(this.content, this.x, this.y, this.width);
 
 }
-
-Object.defineProperties(t_Node.prototype, {
-
-
-})
-
-
-
 
 var Handle_text = {
     lineSeperator: function (input) {
@@ -135,28 +129,23 @@ var Handle_text = {
     FrontKeywordsCount: function (sourceText, keyword) {
         var _count = 0;
         var _text = sourceText;
+        console.log(sourceText);
+        
 
         if (_text.indexOf(keyword) != 0) { return 0 }
-        for (let i = 0; i < _text.length; i++) {
-            if (_text.indexOf(keyword == 0)) {
+        while (_text.indexOf(keyword) == 0) {
                 _count++;
-                _text = _text.slice(keyword.length - 1);
-            } else {
-                break;
-            }
-        }
+                _text = _text.slice(1);
+            } 
         return _count;
     },
 
     FrontKeywordsFilter: function (sourceText, keyword) {
         var _text = sourceText;
-        for (let i = 0; i < _text.length; i++) {
-            if (_text.indexOf(keyword == 0)) {
-                _text = _text.slice(keyword.length - 1);
-            } else {
-                break;
-            }
-        }
+        while (_text.indexOf(keyword) == 0) {
+            _count++;
+            _text = _text.slice(1);
+        } 
         return _text;
     },
 
@@ -173,7 +162,7 @@ var Handle_text = {
         //read items in line Seperated Text;
         for (let i = 0; i < _lineSepertedText.length; i++) {
 
-            outputNodes.push(new t_Node(_lineSepertedText[i],outputNodes[outputNodes.length-1]));
+            outputNodes.push(new t_Node(_lineSepertedText[i], outputNodes[outputNodes.length - 1]));
 
         }
 
@@ -182,15 +171,15 @@ var Handle_text = {
 
     },
 
-    TextInGraphic: function (textContent,graphic){
-        if (graphic.type != "rect"){
-            throw('Not support this graphic type :',graphic.type);
+    TextInGraphic: function (textContent, graphic) {
+        if (graphic.type != "rect") {
+            throw ('Not support this graphic type :', graphic.type);
         }
         var s = graphic;
 
         if (textContent) {
 
-            
+
             let textArea_Width = s.width - (margin * 2);
             let textArea_Height = (s.height - (margin * 2)) / singleLetterHeight;
             var renderText = textContent;
@@ -230,7 +219,7 @@ var Handle_text = {
             return renderText;
         }
     },
-    RenderTextInGraphic:function(HandledText){
+    RenderTextInGraphic: function (HandledText) {
 
     }
 
@@ -241,8 +230,8 @@ var Handle_text = {
 
 document.getElementById('sss').onkeypress = function () {
     Text2Diagram();
-    
-    
+
+
 }
 
 function Text2Diagram() {
@@ -260,11 +249,11 @@ function Text2Diagram() {
 
     s = Handle_text.levelGenerator(s);
     s.forEach(i => {
-        if(i!=null){
+        if (i != null) {
             i.draw();
         }
-        
-    });  
+
+    });
 
 
 }
